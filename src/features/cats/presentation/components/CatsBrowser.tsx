cf75ecf43748039e62af3803698b5c169e09b1c4
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-04-18 23:05:19 +0800

    Used the breeds context to populate the select dropdown for the breeds and used the select breed context to set the selected breed.

--- src/features/cats/presentation/components/CatsBrowser.tsx
+++ src/features/cats/presentation/components/CatsBrowser.tsx
@@ -1,38 +1,53 @@
-import React from 'react';
-import { Row, Button, Col, Form } from 'react-bootstrap'
-import CatCard from "./CatCard";
+import React, { Dispatch, SetStateAction, useContext } from 'react';
+import { Row, Button, Col, Form } from 'react-bootstrap';
 
-// TODO: Populate dynamically the breeds
+import CatCard from './CatCard';
+import Breed from '../../data/models/Breed';
+import { BreedsContext, SelectBreedContext } from '../../providers/BreedsProvider';
+
 // TODO: Populate the dynamically the available images for the breed
 // TODO: Create load more mechanism to load more items.
 
-class CatsBrowser extends React.Component<any, any> {
-  render() {
-    return (
-      <>
-        <br/>
-        <Row>
-          <Col md={3} sm={6} className="col-12">
-            <Form.Group controlId="breed">
-              <Form.Label>Breed</Form.Label>
-              <Form.Control as="select" className="form-select">
-                <option>Breed 1</option>
-                <option>Breed 2</option>
-                <option>Breed 3</option>
-              </Form.Control>
-            </Form.Group>
-          </Col>
-        </Row>
-        <br/>
-        <Row>
-          <CatCard id="awts" imageUrl="https://cdn2.thecatapi.com/images/hBXicehMA.jpg"/>
-        </Row>
-        <div className="d-flex justify-content-center">
-          <Button variant="success">Load More</Button>
-        </div>
-      </>
-    );
+const CatsBrowser: React.FC = () => {
+  const breeds = useContext<Breed[]>(BreedsContext);
+  const setSelectedBreed = useContext<Dispatch<SetStateAction<Breed | undefined>> | null>(SelectBreedContext);
+
+  const selectBreed = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const breedId = e.target.value;
+    const selectedBreed = breeds.find((breed: Breed) => breed.id === breedId);
+
+    if (setSelectedBreed) {
+      setSelectedBreed(selectedBreed);
+    }
   }
+
+  return (
+    <>
+      <br/>
+      <Row>
+        <Col md={3} sm={6} className="col-12">
+          <Form.Group controlId="breed">
+            <Form.Label>Breed</Form.Label>
+            <Form.Control as="select" className="form-select" onChange={selectBreed}>
+              <option>Select Breed</option>
+              {
+                breeds.map((breed: Breed) => (
+                  <option key={breed.id} value={breed.id}>{ breed.name }</option>
+                ))
+              }
+            </Form.Control>
+          </Form.Group>
+        </Col>
+      </Row>
+      <br/>
+      <Row>
+        <CatCard id="awts" imageUrl="https://cdn2.thecatapi.com/images/hBXicehMA.jpg"/>
+      </Row>
+      <div className="d-flex justify-content-center">
+        <Button variant="success">Load More</Button>
+      </div>
+    </>
+  );
 }
 
 export default CatsBrowser;
