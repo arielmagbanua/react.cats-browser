cc26ac3b3f269a43f5aa36fa2c1ac4bd1d731754
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-04-20 00:04:49 +0800

    Loaded the details of the breed of the certain cat.

--- src/features/cats/presentation/components/CatDetails.tsx
+++ src/features/cats/presentation/components/CatDetails.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import { Button, Card, Container } from "react-bootstrap";
+import Breed from "../../data/models/Breed";
+import { SelectedBreedContext } from "../../providers/BreedsProvider";
 
 interface Params {
   id: string
 }
 
 const CatCard: React.FC = () => {
+  const currentBreed = useContext<Breed | undefined | null>(SelectedBreedContext);
+
   const { id } = useParams<Params>();
   const history = useHistory<History>();
 
@@ -19,19 +23,27 @@ const CatCard: React.FC = () => {
       <br/>
       <Card>
         <Card.Header>
-          <Button variant="primary" onClick={goBack}>Back</Button>
+          <Button variant="primary" onClick={goBack}>
+            Back
+          </Button>
         </Card.Header>
         <Card.Img variant="top" src={`https://cdn2.thecatapi.com/images/${id}.jpg`}/>
         <Card.Body>
-          <Card.Title as="h4">Foo Breed</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted" as="h5">Origin: United States</Card.Subtitle>
-          <Card.Subtitle className="mb-2 text-muted" as="h6">Intelligent, Interactive, Lively, Playful, Sensitive</Card.Subtitle>
+          <Card.Title as="h4">
+            { currentBreed && currentBreed.name }
+          </Card.Title>
+          <Card.Subtitle className="mb-2 text-muted" as="h5">
+            Origin: { currentBreed && currentBreed.origin }
+          </Card.Subtitle>
+          <Card.Subtitle className="mb-2 text-muted" as="h6">
+            { currentBreed && currentBreed.temperament }
+          </Card.Subtitle>
           <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
+            { currentBreed && currentBreed.description }
           </Card.Text>
         </Card.Body>
       </Card>
+      <br/>
     </Container>
   );
 }
