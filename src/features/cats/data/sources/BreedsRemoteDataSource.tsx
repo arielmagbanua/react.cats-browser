55e2ca08a26f26e366c23a59210c6b889b6f3010
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-04-20 21:42:42 +0800

    Extracted the data source contract.

--- src/features/cats/data/sources/BreedsRemoteDataSource.tsx
+++ src/features/cats/data/sources/BreedsRemoteDataSource.tsx
@@ -1,40 +1,12 @@
 import http from 'axios';
 import Breed from '../models/Breed';
 import BreedImage from '../models/BreedImage';
+import CatsBreedDataSourceContract from './CatsBreedDataSourceContract';
 
 /**
- * The breed data source interface / contract for abstracting any data source for the breeds
+ * The cat breed remote data source interface / contract for abstracting any data source for the breeds
  */
-export abstract class BreedRemoteDataSourceContract {
-
-  /**
-   * Retrieves all breeds.
-   *
-   * @returns Promise<Breed[]> which is promise that resolves to an array of breed.
-   */
-  abstract getBreeds(): Promise<Breed[]>;
-
-  /**
-   * Retrieves a specific breed.
-   *
-   * @param id The id of the breed
-   *
-   * @returns Promise<Breed> which is a promise that resolves to a breed object.
-   */
-  abstract getBreed(id: string): Promise<Breed>;
-
-  /**
-   * Retrieves images data for a breed.
-   *
-   * @param id The id of the breed.
-   * @param page The page number.
-   * @param limit The maximum number of results that will be returned.
-   */
-  abstract getBreedImages(id: string, page: number, limit: number): Promise<BreedImage[]>;
-}
-
-class BreedsRemoteDataSource implements BreedRemoteDataSourceContract {
-
+class BreedsRemoteDataSource implements CatsBreedDataSourceContract {
   /**
    * Retrieves all cat breeds.
    *
