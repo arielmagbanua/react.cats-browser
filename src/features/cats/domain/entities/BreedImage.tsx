d3150ae15da566e17b7cf00597b9d64428345304
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-04-21 19:59:29 +0800

    Added entities.

---
+++ src/features/cats/domain/entities/BreedImage.tsx
@@ -0,0 +1,34 @@
+/**
+ * BreedImage Entity
+ */
+interface BreedImage {
+  /**
+   * The id of the breed
+   *
+   * @type {string}
+   */
+  id: string
+
+  /**
+   * The url of the breed image.
+   *
+   * @type {string}
+   */
+  url: string,
+
+  /**
+   * The width of the image.
+   *
+   * @type {number}
+   */
+  width: number
+
+  /**
+   * The height of the image.
+   *
+   * @type {number}
+   */
+  height: number
+}
+
+export default BreedImage;
