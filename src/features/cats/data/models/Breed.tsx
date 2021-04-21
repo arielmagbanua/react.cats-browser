46f471f55c644b6dae758a6c184b1e4b2c9c6985
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-04-21 20:13:17 +0800

    Transformed to class and implemented the equivalent entities.

--- src/features/cats/data/models/Breed.tsx
+++ src/features/cats/data/models/Breed.tsx
@@ -1,47 +1,76 @@
+import BreedEntity from '../../domain/entities/Breed';
+
 /**
  * Breed Model
  */
-interface Breed {
+class Breed implements BreedEntity {
   /**
    * The id of the breed
    *
    * @type {string}
    */
-  id: string
+  id: string;
 
   /**
    * The name of the breed
    *
    * @type {string}
    */
-  name: string,
+  name: string;
 
   /**
    * The temperaments of the breed. This is typically comma separated string.
    *
    * @type {string}
    */
-  temperament: string,
+  temperament: string;
 
   /**
    * The description of the breed.
    *
    * @type {string}
    */
-  description: string,
+  description: string;
 
   /**
    * The country of origin of the breed.
    *
    * @type {string}
    */
-  origin: string,
+  origin: string;
 
   /**
    * The reference image of a cat that represents this breed.
    *
+   * @type {string}
    */
-  referenceImageId?: string,
+  referenceImageId?: string;
+
+  /**
+   * Constructor
+   *
+   * @param id
+   * @param name
+   * @param temperament
+   * @param description
+   * @param origin
+   * @param referenceImageId
+   */
+  constructor(
+    id: string,
+    name: string,
+    temperament: string,
+    description: string,
+    origin: string,
+    referenceImageId: string
+  ) {
+    this.id = id;
+    this.name = name;
+    this.temperament = temperament;
+    this.description = description;
+    this.origin = origin;
+    this.referenceImageId = referenceImageId;
+  }
 }
 
 export default Breed;
