6b2099c0e34f54f2165ab515774d2d7599ed1db6
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-04-22 22:29:55 +0800

    Added getting of breed data associated of the breed image.

--- src/features/cats/presentation/pages/CatBreedDetailsPage.tsx
+++ src/features/cats/presentation/pages/CatBreedDetailsPage.tsx
@@ -1,30 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import { Button, Card, Container } from 'react-bootstrap';
 
-import { useSelectedBreedContext } from '../providers/BreedsProvider';
-
-interface Params {
-  id: string
-}
+import { getBreedImage } from '../../domain/use-cases/CatBreedsUseCases';
+import Breed from '../../domain/entities/Breed';
+import BreedImage from '../../domain/entities/BreedImage';
 
 /**
  * The cat breed details page.
  * This page component display the details of cat breed with the image of the cat.
  */
 const CatBreedDetailsPage: React.FC = () => {
-  const currentBreed = useSelectedBreedContext();
+  // local state for the breed of the cat image
+  const [breed, setBreed] = useState<Breed | null>(null);
 
-  const { id } = useParams<Params>();
+  // hook for image id url params
+  const { id } = useParams<{id: string}>();
+
+  // hook for navigation history
   const history = useHistory<History>();
 
-  const goBack = () => {
-    if (currentBreed) {
-      history.push({
-        pathname: '/',
-        search: `?breed=${currentBreed.id}`
+  useEffect(() => {
+    getBreedImage.execute(id)
+      .then((breedImage: BreedImage) => {
+        if (breedImage.breeds !== undefined) {
+          setBreed(breedImage.breeds[0]);
+        }
       });
-    }
+  }, [id]);
+
+  const goBack = () => {
+    const breedParam = breed ? `?breed=${breed.id}` : '';
+
+    history.push({
+      pathname: '/',
+      search: breedParam
+    });
   }
 
   return (
@@ -39,16 +50,16 @@ const CatBreedDetailsPage: React.FC = () => {
         <Card.Img variant="top" src={`https://cdn2.thecatapi.com/images/${id}.jpg`}/>
         <Card.Body>
           <Card.Title as="h4">
-            { currentBreed && currentBreed.name }
+            { breed && breed.name }
           </Card.Title>
           <Card.Subtitle className="mb-2 text-muted" as="h5">
-            Origin: { currentBreed && currentBreed.origin }
+            Origin: { breed && breed.origin }
           </Card.Subtitle>
           <Card.Subtitle className="mb-2 text-muted" as="h6">
-            { currentBreed && currentBreed.temperament }
+            { breed && breed.temperament }
           </Card.Subtitle>
           <Card.Text>
-            { currentBreed && currentBreed.description }
+            { breed && breed.description }
           </Card.Text>
         </Card.Body>
       </Card>
