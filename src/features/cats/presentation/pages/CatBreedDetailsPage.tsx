0e9d11fe763db56c4319e1ef2e585157d68fe491
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-04-22 22:52:47 +0800

    Added handling of non existing image id.

--- src/features/cats/presentation/pages/CatBreedDetailsPage.tsx
+++ src/features/cats/presentation/pages/CatBreedDetailsPage.tsx
@@ -5,6 +5,7 @@ import { Button, Card, Container } from 'react-bootstrap';
 import { getBreedImage } from '../../domain/use-cases/CatBreedsUseCases';
 import Breed from '../../domain/entities/Breed';
 import BreedImage from '../../domain/entities/BreedImage';
+import ErrorMessage from '../../../../shared/components/ErrorMessage';
 
 /**
  * The cat breed details page.
@@ -13,6 +14,9 @@ import BreedImage from '../../domain/entities/BreedImage';
 const CatBreedDetailsPage: React.FC = () => {
   // local state for the breed of the cat image
   const [breed, setBreed] = useState<Breed | null>(null);
+
+  // local state for network error
+  const [networkErrorHappened, setNetworkErrorHappened] = useState<boolean>(false);
 
   // hook for image id url params
   const { id } = useParams<{id: string}>();
@@ -26,6 +30,9 @@ const CatBreedDetailsPage: React.FC = () => {
         if (breedImage.breeds !== undefined) {
           setBreed(breedImage.breeds[0]);
         }
+      })
+      .catch(() => {
+        setNetworkErrorHappened(true);
       });
   }, [id]);
 
@@ -38,33 +45,65 @@ const CatBreedDetailsPage: React.FC = () => {
     });
   }
 
+  const renderDetails = () => {
+    return (
+      <Container>
+        <br/>
+        <Card>
+          <Card.Header>
+            <Button variant="primary" onClick={goBack}>
+              Back
+            </Button>
+          </Card.Header>
+          <Card.Img variant="top" src={`https://cdn2.thecatapi.com/images/${id}.jpg`}/>
+          <Card.Body>
+            <Card.Title as="h4">
+              { breed && breed.name }
+            </Card.Title>
+            <Card.Subtitle className="mb-2 text-muted" as="h5">
+              Origin: { breed && breed.origin }
+            </Card.Subtitle>
+            <Card.Subtitle className="mb-2 text-muted" as="h6">
+              { breed && breed.temperament }
+            </Card.Subtitle>
+            <Card.Text>
+              { breed && breed.description }
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br/>
+      </Container>
+    );
+  }
+
+  const renderErrorMessage = () => {
+    return (
+      <>
+        <br/>
+        <ErrorMessage
+          title="Cat Error"
+          message="Apologies but the breed associated for the image does not exist! Meow!"
+        />
+        <br/>
+      </>
+    );
+  }
+
+  const renderNoData = () => {
+    return (
+      <>
+        <br/>
+        <p>No data available.</p>
+        <br/>
+      </>
+    );
+  }
+
   return (
-    <Container>
-      <br/>
-      <Card>
-        <Card.Header>
-          <Button variant="primary" onClick={goBack}>
-            Back
-          </Button>
-        </Card.Header>
-        <Card.Img variant="top" src={`https://cdn2.thecatapi.com/images/${id}.jpg`}/>
-        <Card.Body>
-          <Card.Title as="h4">
-            { breed && breed.name }
-          </Card.Title>
-          <Card.Subtitle className="mb-2 text-muted" as="h5">
-            Origin: { breed && breed.origin }
-          </Card.Subtitle>
-          <Card.Subtitle className="mb-2 text-muted" as="h6">
-            { breed && breed.temperament }
-          </Card.Subtitle>
-          <Card.Text>
-            { breed && breed.description }
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <br/>
-    </Container>
+    <>
+      { networkErrorHappened ? renderErrorMessage() : renderNoData() }
+      { breed && renderDetails() }
+    </>
   );
 }
 
