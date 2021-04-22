25d7b7267552c232456c67af196a8f5821b758d6
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-04-22 22:25:49 +0800

    changed the prop name for image id.

--- src/features/cats/presentation/components/CatCard.tsx
+++ src/features/cats/presentation/components/CatCard.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import styles from './CatCard.module.scss';
 
 interface IProps {
-  id: string
+  imageId: string
   imageUrl: string,
 }
 
@@ -15,14 +15,14 @@ interface IProps {
  * @param id The id of the cat breed.
  * @param imageUrl The image url of the cat.
  */
-const CatCard: React.FC<IProps> = ({ id, imageUrl }) => {
+const CatCard: React.FC<IProps> = ({ imageId, imageUrl }) => {
   return (
     <Col md={3} className="col-12 mb-3">
       <Card className={styles['image-card']}>
         <Card.Img variant="top" src={imageUrl} width="300" height="300"/>
         <Card.Body>
           <Row className="px-3">
-            <Link to={`/${id}`} className="btn btn-primary">View Details</Link>
+            <Link to={`/${imageId}`} className="btn btn-primary">View Details</Link>
           </Row>
         </Card.Body>
       </Card>
